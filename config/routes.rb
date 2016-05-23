35db8fa2c254d5d84d52bd292d6add6eab305aed
Ashleypettit <ashley@digital380.com.au>
2016-05-23 10:55:14 +1000

    basic css

--- config/routes.rb
+++ config/routes.rb
@@ -5,9 +5,11 @@ Rails.application.routes.draw do
   get '/login' => 'login#new'
   patch '/login' => 'login#update'
   get '/login/profile' => 'login#profile'
-  
+
   get '/agenda/index' => 'agenda#index'
   get 'event_index' => 'eventquestions#index'
   get 'edit_profile' => 'login#edit'
+
+  get 'about' => 'about#index'
 
 end
