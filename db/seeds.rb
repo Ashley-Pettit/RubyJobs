b9bbe78a2321bbeff1f7baeba05c2fbf57138960
Matt McKenzie <mattmckenzie@hotmail.com>
2016-05-23 14:24:20 +1000

    seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+code_arr = ['qwer', 'wert', 'erty', 'rtyu', 'tyui', 'yuio', 'uiop', 'asdf', 'sdfg', 'dfgh', 'fghj', 'ghjk', 'hjkl', 'zxcv', 'xcvb', 'cvbn', 'vbnm']
+
+# country_list.each do |name, population|
+#   Country.create( name: name, population: population )
+# end
+
+code_arr.each do |code|
+  User.create( code: code )
+end
