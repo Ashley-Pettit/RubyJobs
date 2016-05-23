dd648eb7c84035705aedd3c61a8d9eb78200ede1
Ashleypettit <ashley@digital380.com.au>
2016-05-23 10:15:50 +1000

    git update

--- app/controllers/agenda_controller.rb
+++ app/controllers/agenda_controller.rb
@@ -1,8 +1,6 @@
 class AgendaController < ApplicationController
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
 
-def index
-end
+  def index
+  end
 
 end
--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -1,74 +1,20 @@
 class LoginController < ApplicationController
-  before_action :set_login, only: [:show, :edit, :update, :destroy]
-
-  # GET /logins
-  # GET /logins.json
   def index
-    @login = Login.all
   end
 
-  # GET /logins/1
-  # GET /logins/1.json
-  def show
-  end
-
-  # GET /logins/new
   def new
-    @login = Login.new
-  end
-
-  # GET /logins/1/edit
-  def edit
-  end
-
-  # POST /logins
-  # POST /logins.json
-  def create
-    @login = Login.new(login_params)
-
-    respond_to do |format|
-      if @login.save
-        format.html { redirect_to @login, notice: 'Login was successfully created.' }
-        format.json { render :show, status: :created, location: @login }
-      else
-        format.html { render :new }
-        format.json { render json: @login.errors, status: :unprocessable_entity }
-      end
+    puts params[:input_code]
+    if true
+      #Send the user to the profile page
+      redirect_to '/login/profile'
+    else
+      #ADD ERROR MSG AND STAY ON SAME PAGE
     end
   end
 
-  # PATCH/PUT /logins/1
-  # PATCH/PUT /logins/1.json
   def update
-    respond_to do |format|
-      if @login.update(login_params)
-        format.html { redirect_to @login, notice: 'Login was successfully updated.' }
-        format.json { render :show, status: :ok, location: @login }
-      else
-        format.html { render :edit }
-        format.json { render json: @login.errors, status: :unprocessable_entity }
-      end
-    end
+    #updates the DB HERE
+    redirect_to '/agenda/index'
   end
 
-  # DELETE /logins/1
-  # DELETE /logins/1.json
-  def destroy
-    @login.destroy
-    respond_to do |format|
-      format.html { redirect_to login_url, notice: 'Login was successfully destroyed.' }
-      format.json { head :no_content }
-    end
-  end
-
-  private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_login
-      @login = Login.find(params[:id])
-    end
-
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def login_params
-      params.fetch(:login, {})
-    end
 end
