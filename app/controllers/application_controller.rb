752cfec31664c52fc466f6a701d17e33c94e62fe
Matt McKenzie <mattmckenzie@hotmail.com>
2016-05-24 17:36:28 +1000

    Various admin changes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  helper_method :logged_in?, :page_title, :find_user_name, :prepend_dash_to_user_name
+  helper_method :logged_in?, :page_title, :find_user_name, :prepend_dash_to_user_name, :find_user_code
 
   def logged_in?
     session[:user_id]
@@ -9,6 +9,12 @@ class ApplicationController < ActionController::Base
   def find_user_name
     if logged_in?
       User.find_by(id: session[:user_id]).name
+    end
+  end
+
+  def find_user_code
+    if logged_in?
+      User.find_by(id: session[:user_id]).code
     end
   end
 
