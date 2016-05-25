8ea5887e8ae5d8ab6b172c2d64d35dd84925aebf
Ashleypettit <ashley@digital380.com.au>
2016-05-25 17:49:51 +1000

    adding questions

--- app/controllers/agenda_controller.rb
+++ app/controllers/agenda_controller.rb
@@ -4,4 +4,14 @@ class AgendaController < ApplicationController
     @events = Event.all
   end
 
+  def show
+    @event = Event.find_by_id(params[:id])
+    @questions = Question.all
+    @users = User.all
+  end
+
+  def create
+
+  end
+
 end
