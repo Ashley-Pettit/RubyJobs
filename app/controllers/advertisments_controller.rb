adbcc5981180d15089dde1396d5a8dc16bacd87c
Ashley-Pettit <ashley@digital380.com.au>
2016-07-07 17:06:49 +1000

    front-end works

---
+++ app/controllers/advertisments_controller.rb
@@ -0,0 +1,25 @@
+class AdvertismentsController < ApplicationController
+
+  def index
+
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def delete
+  end
+
+end
